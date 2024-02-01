49155f57870fc176422422f733e7c810c2f302f4
Axel <augisax@gmail.com>
2024-02-01 14:36:23 +0100

    Correct register form

--- src/js/login.js
+++ src/js/login.js
@@ -1,5 +1,6 @@
 const registerWrapper = document.querySelector('.register-wrapper');
 const registerButton = document.querySelector('.register-login-button');
+const loginForm = document.getElementById('login-form');
 
 const closeCross = document.querySelector('.close-cross');
 
